9e90b7d8290ea103502b1e5695bc2b431983e22b
dappa <94505216+ReyyNada@users.noreply.github.com>
2023-12-10 14:00:19 +0700

    Update autoleave.py

--- AnonXMusic/plugins/misc/autoleave.py
+++ AnonXMusic/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001686672798
-                                and i.chat.id != -1001549206010
+                                and i.chat.id != -1001897476603
+                                and i.chat.id != -1001704645461
                             ):
                                 if left == 20:
                                     continue
